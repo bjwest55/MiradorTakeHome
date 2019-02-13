ecdf828bc5433852602084aeac86130d5b7e198c
Brandon West <brandonjamesw@gmail.com>
2019-02-12 16:21:29 -0800

    adding some explanation of moving to persistant

--- src/main/java/com/brandonwest/ResponseHolder.java
+++ src/main/java/com/brandonwest/ResponseHolder.java
@@ -4,7 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 //Local holding of cached responses.
-//TODO expand this to use a database of some sort instead.
+//In order for non memory based storage You'd need some sort of database connection
+//So get Responses would instead be an interface to pull from the database and
+//you'd need a new method putResponses to place things into the database.
+
+//You could also cache them here and periodically on a timer update the database.
+//which would cut down on the database calls but would leave you able to lose some data
 public enum ResponseHolder {
     instance;
     private Map<String, ResponseMessage> responses = new HashMap<>();
